235e3baf03cdc9869016a53c853e183bf660ccca
Bala <balag@outlook.com>
2018-11-16 21:08:23 -0800

    3

--- python/worker.py
+++ python/worker.py
@@ -1,5 +1,9 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+cwd = os.getcwd()
+
 with open("pythonworker-worker.txt", "a") as myfile:
-    myfile.write('before1 python/worker.py main.main()')
+    myfile.write("dir_path = '{0}' cwd = '{1}'".format(dir_path , cwd))
 
 from azure.functions_worker import main
 
